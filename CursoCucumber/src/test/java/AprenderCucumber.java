2e0c41475f2cf6a71119371c6c03511a4d3a0d68
Jadson Feitosa <jadson.feitosa.silva@gmail.com>
2021-06-18 22:17:08 -0300

    Estrutura inicial

---
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,20 @@
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class AprenderCucumber {
+	
+	@Dado("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+//	   System.out.println("passou aq");
+	}
+
+	@Quando("^executa-lo$")
+	public void executa_lo() throws Throwable {
+	}
+
+	@Então("^a especificacao deve finalizar com sucesso$")
+	public void a_especificacao_deve_finalizar_com_sucesso() throws Throwable {
+	}
+
+}
---
+++ CursoCucumber/src/test/java/aprender_cucumber.feature
@@ -0,0 +1,12 @@
+# language: pt
+
+Funcionalidade: Aprender Cucumber
+	Como um usuário
+	Eu quero aprender a utilizar cucumber
+	Para que eu possa automatizar criterios de aceiração
+	
+Cenário: Deve executar especificação
+	Dado que criei o arquivo corretamente
+	Quando executa-lo
+	Então a especificacao deve finalizar com sucesso 
+	
